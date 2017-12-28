8349982a657ac0d461897f6268a80f645e66de2c
jianbo <iamxjb@gmail.com>
2017-12-28 16:24:55 +0800

    1、修复我的评论中评论文字太长不换行的问题。 2、修复web-view分享的页面的url链接有问号字符时跳转路径不正常的问题。（微信会只保留问号以前的链接）

--- pages/webpage/webpage.js
+++ pages/webpage/webpage.js
@@ -11,52 +11,80 @@
  */
 
 
-import config from '../../utils/config.js';
+import config from '../../utils/config.js'
+var Api = require('../../utils/api.js');
+var util = require('../../utils/util.js');
+var auth = require('../../utils/auth.js');
+var WxParse = require('../../wxParse/wxParse.js');
+var wxApi = require('../../utils/wxApi.js');
+var wxRequest = require('../../utils/wxRequest.js');
 
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-     url: null
-  
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        url: null,
+        title: "",
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-      var self = this;
-      console.log(decodeURIComponent(options.url));
-      var url = decodeURIComponent(options.url);
-      if (options.url != null) {
+    },
 
-          self.setData({
-              url: url
-          });
-      }
-      else
-      {
-          self.setData({
-              url: 'https://' + config.getDomain
-          });
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        var self = this;
+        console.log(decodeURIComponent(options.url));
+        console.log(options);
+        if (options.url != null) {
+            var url = decodeURIComponent(options.url);
+            if (url.indexOf('*') != -1) {
+                url = url.replace("*", "?");
+            }
+            self.setData({
+                url: url
+            });
+            //   var slug = util.GetUrlFileName(options.url, domain);
+            //   var domain = config.getDomain;
+            //   var title = "";
+            //   if (slug != 'index') {
+            //       var getPostSlugRequest = wxRequest.getRequest(Api.getPostBySlug(slug));
+            //       getPostSlugRequest
+            //           .then(res => {
+            //               if (res.statusCode == 200) {
+            //                   if (res.data.length != 0) {
+            //                       if (res.data[0].title.rendered) {
+            //                           title = ':' + res.data[0].title.rendered;
+            //                           self.setData({
+            //                               title: title
+            //                           });
+            //                           console.log(title);
+            //                       }
+            //                   }
+            //               }
 
-      }
+            //           }) 
+            //   }
+        }
+        else {
+            self.setData({
+                url: 'https://' + config.getDomain
+            });
+        }
 
-  },
-  onShow:function(options)
-  {
-  },
-  
-  
-  onShareAppMessage: function (options){
-        var self =this;
+    },
+    onShareAppMessage: function (options) {
+        var self = this;
+        var url = options.webViewUrl;
+        if (url.indexOf("?") != -1) {
+            url = url.replace("?", "*");
+        }
+        url = 'pages/webpage/webpage?url=' + url;
         console.log(options.webViewUrl);
-        var url = 'pages/webpage/webpage?url=' + options.webViewUrl;       
         return {
-            title: '分享"' + config.getWebsiteName + '"的文章',
+            title: '分享"' + config.getWebsiteName + '"的文章' + self.data.title,
             path: url,
             success: function (res) {
                 // 转发成功
@@ -65,11 +93,6 @@ Page({
             fail: function (res) {
                 // 转发失败
             }
-
-
         }
-        
     }
-  
-  
 })
