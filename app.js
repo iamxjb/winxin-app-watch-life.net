e1818bf19a79eb742b0d85b2676a491ccffceb3a
jianbo <20397795@qq.com>
2019-07-01 11:46:38 +0800

    加入更新提示

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-    
+    // 小程序主动更新
+    that.updateManager();
 
     
   },
@@ -38,12 +39,40 @@ App({
         }
       })
     }
+  },/*小程序主动更新
+    */
+  updateManager() {
+    if (!wx.canIUse('getUpdateManager')) {
+      return false;
+    }
+    const updateManager = wx.getUpdateManager();
+    updateManager.onCheckForUpdate(function (res) {
+    });
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '有新版本',
+        content: '新版本已经准备好，即将重启',
+        showCancel: false,
+        success(res) {
+          if (res.confirm) {
+            updateManager.applyUpdate()
+          }
+        }
+      });
+    });
+    updateManager.onUpdateFailed(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本下载失败',
+        showCancel: false
+      })
+    });
   },
-  globalData:{
-    userInfo:null,
-    openid:'',
-    isGetUserInfo:false,
-    isGetOpenid:false
+  globalData: {
+    userInfo: null,
+    openid: '',
+    isGetUserInfo: false,
+    isGetOpenid: false
 
   }
 })
