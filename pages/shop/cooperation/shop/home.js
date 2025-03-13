69e7186b4b0734ceb43dc37ed2af5932096ca75f
xjb <20397795@qq.com>
2025-03-13 14:33:06 +0800

    增加合作小店位置信息

--- pages/shop/cooperation/shop/home.js
+++ pages/shop/cooperation/shop/home.js
@@ -14,14 +14,49 @@ Page({
 
     this.getCooperationShopProductList()
   },
-
+// 打开地图
+openMap() {
+  var self = this
+  wx.getFuzzyLocation({
+    type: 'gcj02',
+    success(res) {
+      const latitude = parseFloat(self.data.shopInfo.latitude)
+      const longitude = parseFloat(self.data.shopInfo.longitude)
+      const name= self.data.shopInfo.location;
+      const address = self.data.shopInfo.address;
+      wx.openLocation({
+        latitude,
+        longitude,
+        name,
+        address,
+        scale: 17
+      })
+    }
+  })
+},
   async getCooperationShopProductList(){
     const res = await getCooperationShopProductList({
       storeappid: this.data.storeAppId
     })
     if (res.errcode == 0) {
+      const shopInfo = res.shopInfo
+      var marker = {};
+      var markers = [];
+      if(shopInfo)
+        {
+          marker.latitude = parseFloat(shopInfo.latitude);
+          marker.longitude = parseFloat(shopInfo.longitude);
+          marker.id = 1
+          marker.height = 50;
+          marker.width = 40;
+          marker.title = shopInfo.address;
+          marker.display = "ALWAYS";
+          markers.push(marker);
+        }
       this.setData({
-        productList: res.products || []
+        productList: res.products || [],
+        shopInfo: res.shopInfo|| {},
+        markers: markers
       })
     }
   },
